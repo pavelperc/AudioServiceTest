8b4e34390b8c127efe472a085541d85a6db21821
pavelperc <pavelperc@mail.ru>
2021-01-13 18:54:42 +0300

    Switched from liveData to flow.

--- app/src/main/java/com/example/audioservicetest/ui/MediaPlayerViewModel.kt
+++ app/src/main/java/com/example/audioservicetest/ui/MediaPlayerViewModel.kt
@@ -8,10 +8,12 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.audioservicetest.service.AudioService
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
@@ -22,18 +24,22 @@ class MediaPlayerViewModel(application: Application) : AndroidViewModel(applicat
 
     private var mediaController: MediaControllerCompat? = null
 
-    val isConnected = MutableLiveData<Boolean>().apply { value = false }
+    private val _isConnected = MutableStateFlow(false)
+    val isConnected = _isConnected.asStateFlow()
 
     // warning: doesn't update with the current position change
-    val playbackState = MutableLiveData<PlaybackStateCompat>().apply {
-        value = PlaybackStateCompat.Builder()
+    private val _playbackState = MutableStateFlow<PlaybackStateCompat>(
+        PlaybackStateCompat.Builder()
             .setState(PlaybackStateCompat.STATE_NONE, 0, 1f)
             .build()
-    }
+    )
+    val playbackState = _playbackState.asStateFlow()
 
-    val playbackPosition = MutableLiveData<Long>().apply { value = 0 }
+    private val _playbackPosition = MutableStateFlow<Long>(0)
+    val playbackPosition = _playbackPosition.asStateFlow()
 
-    val mediaMetadata = MutableLiveData<MediaMetadataCompat>().apply { value = null }
+    private val _mediaMetadata = MutableStateFlow<MediaMetadataCompat?>(null)
+    val mediaMetadata = _mediaMetadata.asStateFlow()
 
     // todo maybe move to the audio service?
     private var timer: Timer? = null
@@ -44,22 +50,26 @@ class MediaPlayerViewModel(application: Application) : AndroidViewModel(applicat
 
 
     init {
-        playbackState.observeForever { playbackState ->
-            if (playbackState.state == PlaybackStateCompat.STATE_PLAYING) {
-                val initDelay = (1000 - (playbackState.currentPlayBackPosition % 1000)) + 1
-                checkPlaybackPosition()
-                timer = fixedRateTimer("ProgressTimer", false, initDelay, 1000) {
+        viewModelScope.launch {
+            _playbackState.collect { playbackState ->
+                if (playbackState.state == PlaybackStateCompat.STATE_PLAYING) {
+                    val initDelay = (1000 - (playbackState.currentPlayBackPosition % 1000)) + 1
+                    checkPlaybackPosition()
+                    timer = fixedRateTimer("ProgressTimer", false, initDelay, 1000) {
+                        viewModelScope.launch {
+                            checkPlaybackPosition()
+                        }
+                    }
+                } else {
+                    timer = null
                     checkPlaybackPosition()
                 }
-            } else {
-                timer = null
-                checkPlaybackPosition()
             }
         }
     }
 
-    private fun checkPlaybackPosition() {
-        playbackPosition.postValue(mediaController?.playbackState?.currentPlayBackPosition ?: 0)
+    private suspend fun checkPlaybackPosition() {
+        _playbackPosition.emit(mediaController?.playbackState?.currentPlayBackPosition ?: 0)
     }
 
     private inline val PlaybackStateCompat.currentPlayBackPosition: Long
@@ -70,7 +80,6 @@ class MediaPlayerViewModel(application: Application) : AndroidViewModel(applicat
             position
         }
 
-
     private val connectionCallback = object : MediaBrowserCompat.ConnectionCallback() {
         override fun onConnected() {
             viewModelScope.launch {
@@ -78,19 +87,23 @@ class MediaPlayerViewModel(application: Application) : AndroidViewModel(applicat
                 mediaController =
                     MediaControllerCompat(context, mediaBrowser.sessionToken).also { controller ->
                         controller.registerCallback(controllerCallback)
-                        isConnected.postValue(true)
-                        playbackState.postValue(controller.playbackState)
-                        mediaMetadata.postValue(controller.metadata)
+                        _isConnected.emit(true)
+                        _playbackState.emit(controller.playbackState)
+                        _mediaMetadata.emit(controller.metadata)
                     }
             }
         }
 
         override fun onConnectionSuspended() {
-            isConnected.postValue(false)
+            viewModelScope.launch {
+                _isConnected.emit(false)
+            }
         }
 
         override fun onConnectionFailed() {
-            isConnected.postValue(false)
+            viewModelScope.launch {
+                _isConnected.emit(false)
+            }
         }
     }
 
@@ -111,11 +124,15 @@ class MediaPlayerViewModel(application: Application) : AndroidViewModel(applicat
 
     private val controllerCallback = object : MediaControllerCompat.Callback() {
         override fun onMetadataChanged(metadata: MediaMetadataCompat) {
-            mediaMetadata.postValue(metadata)
+            viewModelScope.launch {
+                _mediaMetadata.emit(metadata)
+            }
         }
 
         override fun onPlaybackStateChanged(state: PlaybackStateCompat) {
-            playbackState.postValue(state)
+            viewModelScope.launch {
+                _playbackState.emit(state)
+            }
         }
     }
 
